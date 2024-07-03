33b6434971948524b59b523136c2780c5426446c
Danash Mahmood <danash.mahmoodof@gmail.com>
2024-07-03 18:18:00 +0100

    player info added

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -6,6 +6,12 @@ const PlayerInfo = (props) => {
         <>
             <h3>This is the player info component</h3>
             <p>Username: {props.playerUserName}</p>
+            <p>Followers: {props.playerFollowers}</p>
+            <p>League: {props.playerLeague}</p>
+            <p>Last online: {props.playerLastOnline}</p>
+            <p>Date joined: {props.playerJoinedTimeStamp}</p>
+            <p>Country: {props.playerCountry.name}</p>
+
         
         </>
     )
