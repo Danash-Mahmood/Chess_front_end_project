4d0be6082fd57bc8277cf91f9522fc3ef878cfe1
Danash Mahmood <danash.mahmoodof@gmail.com>
2024-07-03 15:29:09 +0100

    adding the mvp files for containers and components

---
+++ src/containers/Player.jsx
@@ -0,0 +1,22 @@
+import PlayerGames from "../components/PlayerGames";
+import PlayerInfo from "../components/PlayerInfo"
+import PlayerStats from "../components/PlayerStats"
+
+const Player = () => {
+
+
+
+
+    return(
+        <>
+
+        <PlayerInfo></PlayerInfo>
+
+        <PlayerStats></PlayerStats>
+
+        <PlayerGames></PlayerGames>
+        
+        </>
+    )
+}
+export default Player;
