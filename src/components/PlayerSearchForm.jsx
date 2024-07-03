479fe23df7c166aeea001841cb09ad721b6cc382
Danash Mahmood <danash.mahmoodof@gmail.com>
2024-07-03 16:23:19 +0100

    adding a form that will accept a userName input

--- src/components/PlayerSearchForm.jsx
+++ src/components/PlayerSearchForm.jsx
@@ -1,10 +1,28 @@
-const PlayerSearchForm = () => {
+import { useState } from "react";
+
+const PlayerSearchForm = ({data}) => {
+
+
+    const [playerUserName,setPlayerUserName] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        //loadPlayerStats(playerUserName);
+        //loadPlayerInfo(playerUserName);
+    }
+
 
 
     return(
 
         <>
         <h2>This is the search form component</h2>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder = "enter chess.com username" value={playerUserName} onChange = {(event) => {setPlayerUserName(event.target.value); console.log(event.target)}} ></input>
+            {/* make input window slightly longer */}
+            <input type = "submit" placeholder = "submit" ></input>
+        </form>
         
         
         </>
