2817bb56bf15476fa7d0b4848de19850faa272fd
Sunny Mudhar <sunielmudhar@gmail.com>
2024-07-03 17:09:36 +0100

    Added extra api call and cleaned up functionality

--- src/containers/ChessPlayerContainer.jsx
+++ src/containers/ChessPlayerContainer.jsx
@@ -7,26 +7,37 @@ const ChessPlayerContainer = () => {
 
     const [player ,  setPlayer] = useState({});
     const [playerGames , setPlayerGames] = useState([]);
+    const [playerCountry , setCountry] = useState({});
+
+    const fetchPlayerData = (username) => {
+        fetchPlayer(username);
+        fetchPlayerGames(username);
+    }
 
     const fetchPlayer = async (username) => {
         const response = await fetch(`https://api.chess.com/pub/player/${username}`);
         const playerData = await response.json();
         setPlayer(playerData);
-        fetchPlayerGames(username);
     }
 
     const fetchPlayerGames = async (username) => {
         const response = await fetch(`https://api.chess.com/pub/player/${username}/games/archives`);
         const gameData = await response.json();
-        setPlayerGames(playerGames);
+        setPlayerGames(gameData);
+    }
+
+    const fetchPlayerCountry = async (countryURL) => {
+        const response = await fetch(`${countryURL}`);
+        const countryData = await response.json();
+        setCountry(countryData);
     }
 
     return(
         <>
 
         <h1>This is the container</h1>
-        <Player></Player>
-        <PlayerSearchForm fetchPlayer = {fetchPlayer}/>
+        <Player fetchPlayerCountry = {fetchPlayerCountry}></Player>
+        <PlayerSearchForm fetchPlayerData = {fetchPlayerData}/>
         </>
     )
     
