50b8980af6d2683da7fc9dd2abf00a5d85c2eef2
Danash Mahmood <danash.mahmoodof@gmail.com>
2024-07-04 14:33:16 +0100

    partial props for most recent game

--- src/components/PlayerGames.jsx
+++ src/components/PlayerGames.jsx
@@ -2,6 +2,7 @@ import BootstrapContainer from "./BootstrapContainer"
 import Row from "./Row"
 import { useState } from "react";
 
+
 const PlayerGames = ({playerGames,playerUserName,lastMonthGames}) => {
 
     // For this file you will just need to get the game data similar to the PlayerStats and then just iterate through the number
@@ -18,22 +19,58 @@ const PlayerGames = ({playerGames,playerUserName,lastMonthGames}) => {
         }
     *
     */
+
+    // console.log(lastMonthGames);
     const lastMonthGamesObjects = lastMonthGames.games;
-    console.log(lastMonthGames.games);
+    // console.log(lastMonthGames.games);
 
     const mostRecentGame = lastMonthGamesObjects[lastMonthGames.games.length -1];
-    const secondMostRecentGame = lastMonthGames[lastMonthGames.length -2];
-    const thirdMostRecentGame = lastMonthGames[lastMonthGames.length -3];
+    const secondMostRecentGame = lastMonthGames[lastMonthGames.games.length -2];
+    const thirdMostRecentGame = lastMonthGames[lastMonthGames.games.length -3];
 
     console.log(mostRecentGame);
+
+    // console.log(mostRecentGame.accuracies);
+
+    const whatColourIsUser = (playerUserName,gameBeingShown) => {
+        if(playerUserName === gameBeingShown.black.username){
+            return "black";
+        }
+        else{
+            return "white";
+        }
+    }
+
+    const UserAccuracy = (playerUserName,gameBeingShown) => {
+        const userColour = whatColourIsUser(playerUserName,gameBeingShown)
+        if(gameBeingShown.accuracies === undefined){
+            return "N/A";
+        }
+        else if (userColour === "black"){
+            return gameBeingShown.accuracies.black;
+        }
+        else{
+            return gameBeingShown.accuracies.white;
+        }
+    }
+
+    const timeControl = (game) => {
+        return game.time_class;
+
+    }
 
 
     return(
         <>
-            <h3>This is player games component</h3>
+            
             <BootstrapContainer>
                 <Row>
-                    <p>lastMonthGamesData</p>
+                    <h2>Recent Game 1</h2>
+                    <hr></hr>
+                    <p>Time control: {timeControl(mostRecentGame)}</p>
+                    <p> {`${playerUserName}`} : {whatColourIsUser(playerUserName,mostRecentGame)} pieces</p>
+                    <p>{`${playerUserName}`} accuracy is {UserAccuracy(playerUserName,mostRecentGame)} </p>
+                    {/* <Game lastMonthGames = {lastMonthGames} playerUserName = {playerUserName}></Game> */}
                 </Row>
             </BootstrapContainer>
         </>
